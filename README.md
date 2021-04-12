5c755274731731bf429624b1a3c575dd85544ffa
olee <ashik.mou.907@gmail.com>
2021-04-12 13:52:00 +0600

    update

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-<p align="center"><img src="https://laravel.com/assets/img/components/logo-socialite.svg"></p>
-
 <p align="center">
-<a href="https://github.com/laravel/socialite/actions"><img src="https://github.com/laravel/socialite/workflows/tests/badge.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/socialite"><img src="https://img.shields.io/packagist/dt/laravel/socialite" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/socialite"><img src="https://img.shields.io/packagist/v/laravel/socialite" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/socialite"><img src="https://img.shields.io/packagist/l/laravel/socialite" alt="License"></a>
 </p>
 
 ## Introduction
@@ -12,31 +6,13 @@
 OleeTech Inventory Is Completly ERP Package 
 
 
+## Installation
+<pre> composer require olee/inventory </pre>
 
-Add Service Provider In Config/app.php olee\inventory\InventoryServiceProvider::class,
+## ConFiguration 
+After Instalation Go To Config/app.php And Add Provider 
+Olee\Inventory\InventoryServiceProvider::class,
 
 composer run :php artisan vendor:publish And Select olee/inventory
 
-**We are not accepting new adapters.**
 
-Adapters for other platforms are listed at the community driven [Socialite Providers](https://socialiteproviders.netlify.com/) website.
-
-## Official Documentation
-
-Documentation for Socialite can be found on the [Laravel website](https://laravel.com/docs/socialite).
-
-## Contributing
-
-Thank you for considering contributing to Socialite! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
-
-## Code of Conduct
-
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
-
-## Security Vulnerabilities
-
-Please review [our security policy](https://github.com/laravel/socialite/security/policy) on how to report security vulnerabilities.
-
-## License
-
-Laravel Socialite is open-sourced software licensed under the [MIT license](LICENSE.md).
--- src/views/layouts/layout.blade.php
+++ src/views/layouts/layout.blade.php
@@ -6,38 +6,38 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600,300' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="{{asset('olee/inventory/css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/css/font-awesome.min.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/css/jquery-ui.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/jquery.dataTables.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/jquery-ui.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/jquery.dataTables.min.css')}}">
 
-<link rel="stylesheet" href="{{asset('olee/inventory/css/lightbox.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/lightbox.min.css')}}">
 
-<link rel="stylesheet" href="{{asset('olee/inventory/css/colReorder.dataTables.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/colReorder.dataTables.min.css')}}">
 
-<link rel="stylesheet" href="{{asset('olee/inventory/css/style.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/css/menubar.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/css/tabs.css')}}">
-<link rel="stylesheet" href="{{asset('olee/inventory/css/color.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/menubar.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/tabs.css')}}">
+<link rel="stylesheet" href="{{asset('public/olee/inventory/css/color.css')}}">
  @yield('css')
 </head>
 <body>
 @yield('content') 
 
-<script src="{{ asset('olee/inventory/js/jquery-1.12.4.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/popper.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/jquery-1.12.4.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/popper.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/bootstrap.min.js') }}"></script>
 
 
 <!--  Context Menu -->
 
 <!-- DAtatable -->
-<script src="{{ asset('olee/inventory/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/dataTables.colReorder.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/dataTables.rowReorder.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/jquery-ui.min.js') }}"></script>
-<script src="{{ asset('olee/inventory/js/sweetalert.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/dataTables.colReorder.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/dataTables.rowReorder.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('public/olee/inventory/js/sweetalert.min.js') }}"></script>
 
       
     <script src=
@@ -46,7 +46,7 @@
 
 
 <!-- Scripts -->
-    <script src="{{ asset('olee/inventory/js/inventory.js') }}" defer></script>
+    <script src="{{ asset('public/olee/inventory/js/inventory.js') }}" defer></script>
 @yield('js') 
 </body>
 </html>
